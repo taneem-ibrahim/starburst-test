1ca51ff785a0e75f1d5217511f23bccd11a094c6
Trevor McKay <tmckay@redhat.com>
2020-08-07 11:59:33 -0400

    Add basic creation and deletion tests for presto CR

--- test.sh
+++ test.sh
@@ -8,39 +8,34 @@ MY_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"`)
 
 os::test::junit::declare_suite_start "$MY_SCRIPT"
 
-checkCreate() {
-  local corename
-  local frontname
-
-  # Make sure we've only got 1 (in case we ran in a dirty namespace)
-  os::cmd::try_until_text "oc get pods --no-headers -l app.kubernetes.io/name=perceptilabs-core 2> /dev/null | wc -l" '^1$'
-  os::cmd::try_until_text "oc get pods --no-headers -l app.kubernetes.io/name=perceptilabs-frontend 2> /dev/null | wc -l" '^1$'
-
-  # Make sure the lists are fully populaed, sometimes "items" is not immediately populated
-  os::cmd::try_until_success "oc get pod -l app.kubernetes.io/name=perceptilabs-core -o jsonpath='{.items[0].metadata.name}'"
-  corename=$(oc get pod -l app.kubernetes.io/name=perceptilabs-core -o jsonpath="{.items[0].metadata.name}")
-  os::cmd::try_until_success "oc get pod -l app.kubernetes.io/name=perceptilabs-frontend -o jsonpath='{.items[0].metadata.name}'"
-  frontname=$(oc get pod -l app.kubernetes.io/name=perceptilabs-frontend -o jsonpath="{.items[0].metadata.name}")
-
-  os::cmd::try_until_text "oc get pod $corename -o jsonpath='{.status.containerStatuses[?(@.name==\"core\")].ready}'" "true"
-  os::cmd::try_until_text "oc get pod $frontname -o jsonpath='{.status.containerStatuses[?(@.name==\"frontend\")].ready}'" "true"
-  os::cmd::try_until_success "oc get configmap perceptilabs-license"
-  os::cmd::try_until_success "oc get service perceptilabs-core"
-  os::cmd::try_until_success "oc get service perceptilabs-frontend"
-  os::cmd::try_until_success "oc get route perceptilabs-core"
-  os::cmd::try_until_success "oc get route perceptilabs-frontend"
-
-  # If the endpoint list is non-null then services successfully mapped to pods
-  os::cmd::try_until_not_text "oc get endpoints perceptilabs-core -o=jsonpath='{.subsets}' | wc -c" '^0$'
-  os::cmd::try_until_not_text "oc get endpoints perceptilabs-frontend -o=jsonpath='{.subsets}' | wc -c" '^0$'
-
-  host=$(oc get route perceptilabs-frontend -o jsonpath="{.spec.host}")
-  os::cmd::try_until_text "curl $host" "PerceptiLabs"
-}
-
 testCreate() {
-  os::cmd::expect_success_and_text "oc create -f $MY_DIR/manifests/presto.yaml" 'presto.starburstdata.com/presto-test created'
-  os::cmd::try_until_text "oc get pods --no-headers -l instance=presto-test 2>/dev/null | wc -l" '^3$'
+    local coordinator
+    local worker
+    local jpath
+
+    os::cmd::expect_success_and_text "oc create -f $MY_DIR/manifests/presto.yaml" 'presto.starburstdata.com/presto-test created'
+    os::cmd::try_until_text "oc get pods --no-headers -l instance=presto-test 2>/dev/null | wc -l" '^3$'
+
+    # Make sure the pods are all running
+    # Make sure the lists are fully populated first, sometimes "items" is not immediately populated
+    os::cmd::try_until_success "oc get pod -l instance=presto-test -l role=coordinator -o jsonpath='{.items[0].metadata.name}'"
+    coordinator=$(oc get pod -l instance=presto-test -l role=coordinator -o jsonpath='{.items[0].metadata.name}')
+    os::cmd::try_until_text "oc get pod $coordinator -o jsonpath='{.status.phase}'" "Running"
+    for idx in 0 1
+    do
+        os::cmd::try_until_success "oc get pod -l instance=presto-test -l role=worker -o jsonpath='{.items[$idx].metadata.name}'"
+        jpath="{.items[$idx].metadata.name}"
+        worker=$(oc get pod -l instance=presto-test -l role=worker -o jsonpath=\'$jpath\')
+        os::cmd::try_until_text "oc get pod $coordinator -o jsonpath='{.status.phase}'" "Running"
+    done
+
+    # check for config maps
+    os::cmd::expect_success_and_text "oc get cm --no-headers -l instance=presto-test -l role=catalogs 2>/dev/null | wc -l" '^1$'
+    os::cmd::expect_success_and_text "oc get cm --no-headers -l instance=presto-test -l role=configuration 2>/dev/null | wc -l" '^1$'
+
+    # check for service and successful mapping of endpoints
+    os::cmd::try_until_success "oc get service presto-test"
+    os::cmd::try_until_not_text "oc get endpoints presto-test -o=jsonpath='{.subsets}' | wc -c" '^0$'
 }
 
 testDelete() {
@@ -57,13 +52,9 @@ testDelete() {
 	else
 	    break
 	fi
+        os::cmd::expect_success_and_text "oc get cm --no-headers -l instance=$presto 2>/dev/null | wc -l" '^0$'
+        os::cmd::try_until_failure "oc get service $presto"
     done
-  # os::cmd::try_until_failure "oc get configmap perceptilabs-license"
-  # os::cmd::try_until_failure "oc get service perceptilabs-core"
-  # os::cmd::try_until_failure "oc get service perceptilabs-frontend"
-  # os::cmd::try_until_failure "oc get route perceptilabs-core"
-  # os::cmd::try_until_failure "oc get route perceptilabs-frontend"
-  # os::cmd::try_until_text "oc get perceptilabs --no-headers 2> /dev/null | wc -l" '^0$'
 }
 
 # make sure we're not starting with any prestos
